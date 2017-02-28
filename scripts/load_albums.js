65fe34c4da2e4a6c0c5f27470910a8ac173eed79
Paco <palmtreerooskee@gmail.com>
2017-02-27 20:33:06 -0500

    ch 4 Learning Node.js

--- scripts/load_albums.js
+++ scripts/load_albums.js
@@ -1,3 +1,4 @@
+
 var http = require('http'),
     fs = require('fs');
 
@@ -41,11 +42,13 @@ function handle_incoming_request(req, res) {
       return;
     }
 
-    var out = {error: null, data: {albums: albums}};
+    var out = {error: null,
+                data: {albums: albums}};
     res.writeHead(200, {"Content-Type": "application/json"});
     res.end(JSON.stringify(out) + "\n");
   });
 }
 
 var s = http.createServer(handle_incoming_request);
+
 s.listen(8080);
