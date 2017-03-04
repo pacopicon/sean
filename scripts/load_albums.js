6a4cbb15cdd90c9bfb20e7349d791cf02501548d
Paco <palmtreerooskee@gmail.com>
2017-03-04 18:13:46 -0500

    Learning to Juggle: Handling More Requests

--- scripts/load_albums.js
+++ scripts/load_albums.js
@@ -6,7 +6,7 @@ function load_album_list(callback) {
 
   fs.readdir("albums", (err, files) => {
     if (err) {
-      callback(err);
+      callback(make_error("file_error", JSON.stringify(err)));
       return;
     }
 
@@ -20,11 +20,12 @@ function load_album_list(callback) {
 
       fs.stat("albums/" + files[index], (err, stats) => {
         if (err) {
-          callback(err);
+          callback(make_error("file_error", JSON.stringify(err)));
           return;
         }
         if (stats.isDirectory()) {
-          only_dirs.push(files[index]);
+          var obj = {name: files[index] };
+          only_dirs.push(obj);
         }
         iterator(index + 1)
       });
@@ -34,9 +35,83 @@ function load_album_list(callback) {
   });
 }
 
+function load_album(album_name, callback) {
+  // we will assume that any directory in our 'albums'
+  // subfolder is an album.
+  fs.readdir("albums/" + album_name, (err, files) => {
+    if (err) {
+      if (err.code == "ENOENT") {
+        callback(no_such_album());
+      } else {
+        callback(make_error("file_error", JSON.stringify(err)));
+      }
+      return;
+    }
+    var only_files = [];
+    var path = `albums/${album_name}/`
+
+    var iterator = (index) => {
+      if (index == files.length) {
+        var obj = {short_name: album_name, photos: only_files};
+        callback(null, obj);
+        return;
+      }
+
+      fs.stat(path + files[index], (err, stats) => {
+        if (err) {
+          callback(make_error("file_error", JSON.stringify(err)));
+          return;
+        }
+        if (stats.isFile()) {
+          var obj = {filename: files[index], desc: files[index]};
+          only_files.push(obj);
+        }
+        iterator(index+1);
+      });
+    }
+    iterator(0);
+  });
+}
+
 function handle_incoming_request(req, res) {
   console.log("INCOMING REQUEST: " + req.method + " " + req.url);
-  load_album_list((err, albums) => {
+
+  if (req.url == '/albums.json') {
+    handle_list_albums(req. res);
+  } else if (req.url.substr(0,7) ==  '/albums' && req.url.substr(req.url.length - 5) == '.json') {
+    handle_get_album(req, res);
+  } else {
+    send_failure(res, 404, invalid_resource());
+  }
+}
+
+function handle_list_albums(req, res) {
+  load_album_list( (err, albums) => {
+    if (err) {
+      send_failure(res, 500, err);
+      return;
+    }
+    send_success(res, {albums: albums});
+  });
+}
+
+function handle_get_album(req, res) {
+  // format of request is /albums/album_name.json
+  var album_name = req.url.substr(7, req.url.length - 12);
+  load_album(album_name, (err, album_contents) => {
+    if (err && err.error == "no_such_album") {
+      send_failure(res, 404, err);
+    } else if (err) {
+      send_failure(res, 500, err);
+    } else {
+      send_success(res, {album_data: album_contents});
+    }
+  });
+}
+
+
+
+load_album_list((err, albums) => {
     if (err) {
       res.writeHead(500, {"Content-Type": "application/json"});
       res.end(JSON.stringify(err) + "\n");
