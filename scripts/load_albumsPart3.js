2e7f285dc45199c9f301786855eb0060f055b07f
pacopicon <palmtreerooskee@gmail.com>
2017-03-05 18:17:14 -0500

    fixing load_albumsPart3.js

--- scripts/load_albumsPart3.js
+++ scripts/load_albumsPart3.js
@@ -1,5 +1,6 @@
 var http = require('http'),
-  fs = require('fs');
+  fs = require('fs'),
+  url = require('url');
 
 function load_album_list(callback) {
   // we will just assume that any directory in our 'albums'
@@ -23,11 +24,9 @@ function load_album_list(callback) {
           callback(make_error("file_error", JSON.stringify(err)));
           return;
         }
-        console.log("is stats a directory?: " + stats.isDirectory());
         if (stats.isDirectory()) {
           var obj = { name: files[index] };
           only_dirs.push(obj);
-          console.log("only_dirs: " + only_dirs);
         }
         iterator(index + 1)
       });
@@ -36,7 +35,7 @@ function load_album_list(callback) {
   });
 }
 
-function load_album(album_name, callback) {
+function load_album(album_name, page, page_size, callback) {
   // we will just assume that any directory in our 'albums'
   // subfolder is an album.
   fs.readdir("albums/" + album_name, (err, files) => {
@@ -50,11 +49,16 @@ function load_album(album_name, callback) {
     }
 
     var only_files = [];
-    var path = `albums/${album_name}/`;
+    var path = "albums/" + album_name + "/";
 
     var iterator = (index) => {
       if (index == files.length) {
-        var obj = { short_name: album_name, photos: only_files };
+        var ps;
+        // slice fails gracefuly if params are out of range
+        var start = page * page_size
+        ps = only_files.slice(start, start + page_size);
+        var obj = { short_name: album_name, photos: ps };
+
         callback(null, obj);
         return;
       }
@@ -64,26 +68,29 @@ function load_album(album_name, callback) {
           callback(make_error("file_error", JSON.stringify(err)));
           return;
         }
-        console.log("is stats a file?: " + stats.isFile());
         if (stats.isFile()) {
           var obj = { filename: files[index], desc: files[index] };
           only_files.push(obj);
-          console.log("only_files: " + only_files);
         }
         iterator(index + 1)
       });
-
     }
     iterator(0);
   });
 }
 
 function handle_incoming_request(req, res) {
-  console.log("INCOMING REQUEST: " + req.method + " " + req.url);
-  if (req.url == '/albums.json') {
+
+  // parse the query params into an object and get the path
+  // without them. (2nd param true = parse the params).
+  req.parsed_url = url.parse(req.url, true);
+  var core_url = req.parsed_url.pathname;
+
+  // test this fixed url to see what they're asking for
+  if (core_url == '/albums.json') {
     handle_list_albums(req, res);
-  } else if (req.url.substr(0, 7) == '/albums'
-           && req.url.substr(req.url.length - 5) == '.json') {
+  } else if (core_url.substr(0, 7) == '/albums'
+           && core_url.substr(core_url.length - 5) == '.json') {
     handle_get_album(req, res);
   } else {
     send_failure(res, 404, invalid_resource());
@@ -101,10 +108,20 @@ function handle_list_albums(req, res) {
 }
 
 function handle_get_album(req, res) {
+  // get the GET params
+  var getp = req.parsed_url.query;
+  var page_num = getp.page ? parseInt(getp.page) : 0;
+  var page_size = getp.page_size ? parseInt(getp.page_size) : 1000;
+
+  if (isNaN(parseInt(page_num))) page_num = 0;
+  if (isNaN(parseInt(page_size))) page_size = 1000;
+
   // format of request is /albums/album_name.json
-  var album_name = req.url.substr(7, req.url.length - 12);
-  console.log("album_name: " + album_name);
-  load_album(album_name, (err, album_contents) => {
+
+  var core_url = req.parsed_url.pathname;
+
+  var album_name = core_url.substr(7, core_url.length - 12);
+  load_album(album_name, page, page_size, (err, album_contents) => {
     if (err && err.error == "no_such_album") {
       send_failure(res, 404, err);
     }  else if (err) {
